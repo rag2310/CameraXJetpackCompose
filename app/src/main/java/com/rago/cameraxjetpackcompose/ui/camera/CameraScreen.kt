1917d72d398808f63b96db4ac54aadb1011ce425
Rodolfo Gutierrez <rgutierrez@trackchain.io>
2022-10-17 16:54:19 -0600

    [ADD] Control Camera

--- app/src/main/java/com/rago/cameraxjetpackcompose/ui/camera/CameraScreen.kt
+++ app/src/main/java/com/rago/cameraxjetpackcompose/ui/camera/CameraScreen.kt
@@ -12,17 +12,27 @@ import androidx.camera.view.PreviewView
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Lens
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -32,7 +42,6 @@ import coil.request.ImageRequest
 import com.rago.cameraxjetpackcompose.getCameraProvider
 import com.rago.cameraxjetpackcompose.takePicture
 import kotlinx.coroutines.launch
-import java.io.File
 
 @Composable
 fun CameraScreen(cameraUIState: CameraUIState) {
@@ -106,34 +115,32 @@ fun CameraScreen(cameraUIState: CameraUIState) {
 
 @Composable
 private fun CameraPreview(
+    modifier: Modifier = Modifier,
     scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
     onUseCase: (UseCase) -> Unit = {}
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        AndroidView(
-            modifier = Modifier.fillMaxSize(),
-            factory = { context ->
-                val previewView = PreviewView(context).apply {
-                    this.scaleType = scaleType
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                    )
-                }
-
-                onUseCase(
-                    Preview.Builder()
-                        .build()
-                        .also {
-                            it.setSurfaceProvider(previewView.surfaceProvider)
-                        }
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
                 )
-
-                previewView
             }
-        )
-    }
 
+            onUseCase(
+                Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
+            )
+
+            previewView
+        }
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -142,8 +149,18 @@ fun CameraCapture(
     cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
     cameraUIState: CameraUIState
 ) {
-    Scaffold(Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize()) {
+    Scaffold(
+        Modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .background(Color.Yellow)
+    ) { paddingValues ->
+        Log.i("CameraScreen", "padding = $paddingValues")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+        ) {
             val context = LocalContext.current
             val lifecycleOwner = LocalLifecycleOwner.current
             var previewUseCase by remember { mutableStateOf<UseCase>(Preview.Builder().build()) }
@@ -156,32 +173,81 @@ fun CameraCapture(
                 )
             }
             val coroutineScope = rememberCoroutineScope()
-
+            val heightAllBoxActions = remember {
+                mutableStateOf(0.dp)
+            }
+            val heightHalfBoxActions = remember {
+                mutableStateOf(0.dp)
+            }
+            val configuration = LocalConfiguration.current
+            val density = LocalDensity.current
 
             CameraPreview(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(configuration.screenHeightDp.dp - heightAllBoxActions.value + paddingValues.calculateBottomPadding() + heightHalfBoxActions.value),
                 onUseCase = {
                     previewUseCase = it
                 }
             )
-            Column(
-                Modifier
+            GalleryPreview(cameraUIState = cameraUIState)
+            Box(
+                modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.Gray.copy(alpha = 0.3f))
                     .align(Alignment.BottomCenter)
-                    .navigationBarsPadding()
-            ) {
-                GalleryPreview(cameraUIState = cameraUIState)
-                Button(
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .padding(16.dp),
-                    onClick = {
-                        coroutineScope.launch {
-                            cameraUIState.newFile(imageCaptureUseCase.takePicture(context))
+                    .onGloballyPositioned { layoutCoordinates ->
+                        heightAllBoxActions.value = with(density) {
+                            layoutCoordinates.size.height.toDp()
                         }
                     }
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(74.dp)
+                        .background(Color.Transparent)
                 ) {
-                    Text("Click!")
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .background(Color.Transparent)
+                            .onGloballyPositioned { layoutCoordinates ->
+                                heightHalfBoxActions.value = with(density) {
+                                    layoutCoordinates.size.height.toDp()
+                                }
+                            }
+                    ) {
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .background(Color.Black)
+                    ) {
+
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    CameraControl(
+                        Icons.Sharp.Lens,
+                        modifier = Modifier
+                            .size(64.dp)
+                            .padding(1.dp)
+                            .border(1.dp, Color.White, CircleShape),
+                        effect = true,
+                        onClick = {
+                            coroutineScope.launch {
+                                cameraUIState.newFile(imageCaptureUseCase.takePicture(context))
+                            }
+                        }
+                    )
                 }
             }
 
@@ -215,6 +281,45 @@ private fun GalleryPreview(cameraUIState: CameraUIState) {
 
                     )
             }
+        }
+    }
+}
+
+@Composable
+fun CameraControl(
+    imageVector: ImageVector,
+    modifier: Modifier = Modifier,
+    effect: Boolean = false,
+    onClick: () -> Unit
+) {
+    if (effect) {
+        val interactionSource = remember { MutableInteractionSource() }
+        val isFocused by interactionSource.collectIsPressedAsState()
+        val color = if (isFocused) Color.Red else Color.Transparent
+
+        IconButton(
+            onClick = onClick,
+            modifier = modifier,
+            interactionSource = interactionSource
+        ) {
+            Icon(
+                imageVector,
+                contentDescription = null,
+                modifier = modifier,
+                tint = color
+            )
+        }
+    } else {
+        IconButton(
+            onClick = onClick,
+            modifier = modifier,
+        ) {
+            Icon(
+                imageVector,
+                contentDescription = null,
+                modifier = modifier,
+                tint = Color.White
+            )
         }
     }
 }
