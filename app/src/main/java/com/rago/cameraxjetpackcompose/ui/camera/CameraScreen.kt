9598710a5a296eeceb7a28559b3242daa8dd161f
Rodolfo Gutierrez <rgutierrez@trackchain.io>
2022-10-17 14:08:30 -0600

    [FEATURE] Show image captured, preview

--- app/src/main/java/com/rago/cameraxjetpackcompose/ui/camera/CameraScreen.kt
+++ app/src/main/java/com/rago/cameraxjetpackcompose/ui/camera/CameraScreen.kt
@@ -1,83 +1,150 @@
 package com.rago.cameraxjetpackcompose.ui.camera
 
+import android.net.Uri
 import android.util.Log
+import android.util.Size
 import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY
 import androidx.camera.core.Preview
+import androidx.camera.core.UseCase
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.net.toUri
+import coil.compose.rememberAsyncImagePainter
+import com.rago.cameraxjetpackcompose.executor
 import com.rago.cameraxjetpackcompose.getCameraProvider
+import com.rago.cameraxjetpackcompose.takePicture
 import kotlinx.coroutines.launch
+import java.io.File
 
 @Composable
 fun CameraScreen() {
-    CameraContent()
+    val emptyImageUri = Uri.parse("file://dev/null")
+    var imageUri by remember { mutableStateOf(emptyImageUri) }
+    if (imageUri != emptyImageUri) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = rememberAsyncImagePainter(imageUri),
+                contentDescription = "Captured image"
+            )
+            Button(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                onClick = {
+                    imageUri = emptyImageUri
+                }
+            ) {
+                Text("Remove image")
+            }
+        }
+    } else
+        CameraCapture(
+            onImageFile = { file ->
+                imageUri = file.toUri()
+            }
+        )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun CameraContent(
+private fun CameraPreview(
     scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
-    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    onUseCase: (UseCase) -> Unit = {}
 ) {
-    Scaffold(Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            val coroutineScope = rememberCoroutineScope()
-            val lifecycleOwner = LocalLifecycleOwner.current
-            AndroidView(
-                modifier = Modifier.fillMaxSize(),
-                factory = { context ->
-                    val previewView = PreviewView(context).apply {
-                        this.scaleType = scaleType
-                        layoutParams = ViewGroup.LayoutParams(
-                            ViewGroup.LayoutParams.MATCH_PARENT,
-                            ViewGroup.LayoutParams.MATCH_PARENT
-                        )
-                    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { context ->
+                val previewView = PreviewView(context).apply {
+                    this.scaleType = scaleType
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                }
 
-                    // CameraX Preview UseCase
-                    val previewUseCase = Preview.Builder()
+                onUseCase(
+                    Preview.Builder()
                         .build()
                         .also {
                             it.setSurfaceProvider(previewView.surfaceProvider)
                         }
+                )
 
-                    coroutineScope.launch {
-                        val cameraProvider = context.getCameraProvider()
-                        val hasCameraSelect = cameraProvider.hasCamera(cameraSelector)
-                        val newCameraSelector = if (hasCameraSelect) {
-                            cameraSelector
-                        } else {
-                            if (cameraSelector.lensFacing == CameraSelector.LENS_FACING_FRONT) {
-                                CameraSelector.DEFAULT_BACK_CAMERA
-                            } else {
-                                CameraSelector.DEFAULT_FRONT_CAMERA
-                            }
-                        }
-                        try {
-                            // Must unbind the use-cases before rebinding them.
-                            cameraProvider.unbindAll()
-                            cameraProvider.bindToLifecycle(
-                                lifecycleOwner,
-                                newCameraSelector,
-                                previewUseCase
-                            )
-                        } catch (ex: Exception) {
-                            Log.e("CameraPreview", "Use case binding failed", ex)
-                        }
-                    }
+                previewView
+            }
+        )
+    }
 
-                    previewView
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CameraCapture(
+    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
+    onImageFile: (File) -> Unit = { }
+) {
+    Scaffold(Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            val context = LocalContext.current
+            val lifecycleOwner = LocalLifecycleOwner.current
+            var previewUseCase by remember { mutableStateOf<UseCase>(Preview.Builder().build()) }
+            val imageCaptureUseCase by remember {
+                mutableStateOf(
+                    ImageCapture.Builder()
+                        .setCaptureMode(CAPTURE_MODE_MAXIMIZE_QUALITY)
+                        .setTargetResolution(Size(720, 720))
+                        .build()
+                )
+            }
+            val coroutineScope = rememberCoroutineScope()
+
+
+            CameraPreview(
+                onUseCase = {
+                    previewUseCase = it
                 }
             )
+            Button(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(16.dp)
+                    .align(Alignment.BottomCenter)
+                    .navigationBarsPadding(),
+                onClick = {
+                    coroutineScope.launch {
+                        onImageFile(imageCaptureUseCase.takePicture(context.executor))
+                    }
+                }
+            ) {
+                Text("Click!")
+            }
+
+            LaunchedEffect(previewUseCase) {
+                val cameraProvider = context.getCameraProvider()
+                try {
+                    // Must unbind the use-cases before rebinding them.
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner, cameraSelector, previewUseCase, imageCaptureUseCase
+                    )
+                } catch (ex: Exception) {
+                    Log.e("CameraCapture", "Failed to bind camera use cases", ex)
+                }
+            }
         }
     }
 }
